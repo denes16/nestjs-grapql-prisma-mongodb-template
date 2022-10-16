c7712b50dcaa64d9302bd678ea624be991aa7ca0
MarcaCriss <cristianmarca70@gmail.com>
2022-10-16 04:46:37 -0400

    feat(auth): test the resolver signup method

--- src/features/auth/test/auth.resolver.spec.ts
+++ src/features/auth/test/auth.resolver.spec.ts
@@ -6,10 +6,10 @@ import { SignInInputMock } from './mocks/singInInput.mock';
 import { PrismaService } from '../../../core/services/prisma/prisma.service';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { SignUpInputMock } from './mocks/signUpInput.mock';
 
 describe('AuthResolver', () => {
   let resolver: AuthResolver;
-  let authService: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -23,17 +23,26 @@ describe('AuthResolver', () => {
     }).compile();
 
     resolver = module.get<AuthResolver>(AuthResolver);
-    authService = module.get<AuthService>(AuthService);
   });
 
   describe('signIn', () => {
     it('should be successful', async () => {
       jest
-        .spyOn(authService, 'signIn')
+        .spyOn(resolver, 'signIn')
         .mockImplementation(
           () => new Promise((resolve) => resolve(AuthResponseMock)),
         );
       expect(await resolver.signIn(SignInInputMock)).toBe(AuthResponseMock);
+    });
+  });
+  describe('signUp', function () {
+    it('should be successful', async () => {
+      jest
+        .spyOn(resolver, 'signUp')
+        .mockImplementation(
+          () => new Promise((resolve) => resolve(AuthResponseMock)),
+        );
+      expect(await resolver.signUp(SignUpInputMock)).toBe(AuthResponseMock);
     });
   });
 });
