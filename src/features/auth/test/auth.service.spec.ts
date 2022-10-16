d15ef05bf729ee50c655050692023e9975f03d0d
MarcaCriss <cristianmarca70@gmail.com>
2022-10-16 03:32:04 -0400

    feat(auth): test the signup method

--- src/features/auth/test/auth.service.spec.ts
+++ src/features/auth/test/auth.service.spec.ts
@@ -3,10 +3,10 @@ import { AuthService } from '../auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../../../core/services/prisma/prisma.service';
 import { SignInInputMock } from './mocks/singInInput.mock';
-import { AuthResponse } from '../models/auth-response.model';
-import { SignInInput } from '../dto/sign-in.input';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { ForbiddenException } from '@nestjs/common';
+import { ConflictException, ForbiddenException } from '@nestjs/common';
+import { AuthResponseMock } from './mocks/authResponse.mock';
+import { SignUpInputMock } from './mocks/signUpInput.mock';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -22,26 +22,12 @@ describe('AuthService', () => {
 
   describe('signIn', () => {
     it('should be successful', async () => {
-      const result: AuthResponse = {
-        accessToken: 'eyJhbGciOiJIU',
-        refreshToken: 'eyJhbGciOiJIU',
-        user: {
-          id: '633653edc8c1e095415877de',
-          firstName: 'cris',
-          lastName: 'marca',
-          email: 'email1@email.com',
-          password: '$2b$10$',
-          language: 'en',
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          isActive: true,
-          modelName: 'User',
-        },
-      };
       jest
         .spyOn(authService, 'signIn')
-        .mockImplementation(() => new Promise((resolve) => resolve(result)));
-      expect(await authService.signIn(SignInInputMock)).toBe(result);
+        .mockImplementation(
+          () => new Promise((resolve) => resolve(AuthResponseMock)),
+        );
+      expect(await authService.signIn(SignInInputMock)).toBe(AuthResponseMock);
     });
     it('should be a bad credential error', () => {
       jest
@@ -54,6 +40,29 @@ describe('AuthService', () => {
         );
       expect(authService.signIn(SignInInputMock)).rejects.toThrow(
         ForbiddenException,
+      );
+    });
+  });
+  describe('signUp', () => {
+    it('should be successful', async () => {
+      jest
+        .spyOn(authService, 'signUp')
+        .mockImplementation(
+          () => new Promise((resolve) => resolve(AuthResponseMock)),
+        );
+      expect(await authService.signUp(SignUpInputMock)).toBe(AuthResponseMock);
+    });
+    it('should be email already exists error', () => {
+      jest
+        .spyOn(authService, 'signUp')
+        .mockImplementation(
+          () =>
+            new Promise((resolve, reject) =>
+              reject(new ConflictException('errors.emailAlreadyExists')),
+            ),
+        );
+      expect(authService.signUp(SignUpInputMock)).rejects.toThrow(
+        ConflictException,
       );
     });
   });
